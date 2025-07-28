687d62c9dac179d503eaaa3f1b6cd24fa2d76970
swaraj <swarajcn774@gmail.com>
2025-07-28 13:45:12 +0530

    qwertyuio

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-/*import express from 'express'
+import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js';
@@ -32,42 +32,6 @@ app.get('/',(req,res)=>{
   res.send('SERVER WORKING')
 })
 
-//app.listen(port,()=>console.log('SERVER IS RUNNING PORT 4000 AND'));  */\
-
-
-
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './config/mongodb.js';
-import connectCloudinary from './config/cloudinary.js';
-import userRouter from './routes/userRoute.js';
-import productRouter from './routes/productRoute.js';
-import cartRouter from './routes/cartRoute.js';
-import orderRouter from './routes/orderRoute.js';
-
-const app = express();
-connectDB();
-connectCloudinary();
-
-app.use(express.json());
-app.use(cors());
-
-app.use('/api/user', userRouter);
-app.use('/api/product', productRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/order', orderRouter);
-
-app.get('/', (req, res) => {
-  res.send('SERVER WORKING');
-});
-
-// ❌ REMOVE THIS FOR VERCEL
-// app.listen(port, () => console.log('RUNNING ON PORT 4000'));
-
-// ✅ DO THIS INSTEAD
-export default app;
-
-
+app.listen(port,()=>console.log('SERVER IS RUNNING PORT 4000 AND')); 
 
 
