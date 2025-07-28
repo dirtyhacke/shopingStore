6927f44cab97ab528fe396a746c3d026bbfdfb42
swaraj <swarajcn774@gmail.com>
2025-07-28 11:39:41 +0530

    serverupdate

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express from 'express'
+/*import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js';
@@ -32,4 +32,41 @@ app.get('/',(req,res)=>{
   res.send('SERVER WORKING')
 })
 
-app.listen(port,()=>console.log('SERVER IS RUNNING PORT 4000 AND'));
+app.listen(port,()=>console.log('SERVER IS RUNNING PORT 4000 AND')); */
+
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
+import cartRouter from './routes/cartRoute.js'
+import orderRouter from './routes/orderRoute.js'
+
+//app config
+
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+// middile wares
+
+app.use(express.json())
+app.use(cors())
+
+// api end points
+app.use('/api/user',userRouter)
+
+app.use('/api/product',productRouter)
+
+app.use('/api/cart',cartRouter)
+app.use('/api/order',orderRouter)
+
+app.get('/',(req,res)=>{
+    res.send("API WORKING")
+})
+
+app.listen(port,()=> console.log("server started on port :" + port))
+
+
